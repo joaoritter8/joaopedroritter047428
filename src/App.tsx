e531dd37fedfaf7d72678d56695804689faefde8
joaoritter8 <joaoritter8@gmail.com>
2026-02-01 16:58:33 -0400

    refactor: formatacao do codigo com eslint/prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-
-
 function App() {
-
   return (
-   <div>
-    <h1>Pets MT</h1>
-   </div>
-  )
+    <div>
+      <h1>Pets MT</h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
